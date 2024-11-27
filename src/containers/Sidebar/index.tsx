2b9e630378c7ad7efcb6bb52ba6dddba21fead3b
Paula Rabelo <paula_rabelo2010@hotmail.com>
2024-11-27 17:41:07 -0300

    estilizando a Sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,24 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { Descricao, Botao, SidebarContainer } from "./styles"
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/paularrabelo.png"/>
-    <Titulo fontSize={20}>Paula Rabelo</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Paula Rabelo</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        paularrabelo
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Dev Full Stack
+      </Descricao>
+      <Botao>
+        Trocar tema
+      </Botao>
+    </SidebarContainer>
   </aside>
 )
 
--- src/styles.ts
+++ src/styles.ts
@@ -10,6 +10,7 @@ const EstiloGlobal = createGlobalStyle`
 
   body {
     padding-top: 80px;
+    padding-bottom: 80px;
 
     @media (max-width: 768px) {
       padding-top: 16px;
@@ -28,12 +29,14 @@ export const Container = styled.div`
   grid-template-columns: 128px auto;
   column-gap: 56px;
 
+
   @media (max-width: 768px) {
     max-width: 80%;
     display: block;
-  }
 
-  img {
-    max-width: 100%;
+    img {
+    height: auto;
+    width: 100%;
+    }
   }
 `
